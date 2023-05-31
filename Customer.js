8d72cdfb8e7a0a424af59ff8f747e40ab1e15130
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2023-05-31 22:19:42 +0500

    try catch

--- Customer.js
+++ Customer.js
@@ -5,36 +5,40 @@ const { default: mongoose } = require("mongoose");
 const Customer = express.Router();
 Customer.post("/Signup", (req, res) => {
   console.log(req.body.username);
-  User.find({ email: req.body.email })
-    .then((doc) => {
-      // console.log(doc.length);
-      if (doc.length >= 1) {
-        res.end("This is Already Exist");
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) throw err;
+  try {
+    User.find({ email: req.body.email })
+      .then((doc) => {
+        // console.log(doc.length);
+        if (doc.length >= 1) {
+          res.end("This is Already Exist");
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) throw err;
 
-          const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
-            status: true,
-          });
-          user
-            .save()
-            .then(() => {
-              res.end("Okey Data Has Been Added");
-            })
-            .catch((err) => {
-              console.log(err);
-              res.end("Somthing Went Wrong");
+            const user = new User({
+              username: req.body.username,
+              email: req.body.email,
+              password: hash,
+              status: true,
             });
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+            user
+              .save()
+              .then(() => {
+                res.end("Okey Data Has Been Added");
+              })
+              .catch((err) => {
+                console.log(err);
+                res.end("Somthing Went Wrong");
+              });
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (error) {
+    console.log(error);
+  }
 });
 Customer.post("/Login", (req, res) => {
   console.log(req.query.email);
