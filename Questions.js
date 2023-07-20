ef77a37178e8cd8ec1647f07ee681dd53aa1cd41
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2023-07-20 17:29:59 +0500

    Data has been Added

--- Questions.js
+++ Questions.js
@@ -2,140 +2,141 @@ const express = require('express');
 const { model } = require('mongoose');
 const Questios = require('./modal/User');
 const Category = require('./modal/Category');
+const { initializeApp } = require('firebase/app')
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require('firebase/storage');
 const Route = express.Router()
 const multer = require('multer');
 const Paper = require('./modal/Paper');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Specify the folder where you want to store the uploaded images
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
-    }
-  });
-  const upload = multer({ storage: storage });
+const firebaseConfig = {
+  apiKey: "AIzaSyACQGtdj4qFxD6QPiP0omlaOBkz7zTMJMI",
+  authDomain: "ppsc-db25f.firebaseapp.com",
+  projectId: "ppsc-db25f",
+  storageBucket: "ppsc-db25f.appspot.com",
+  messagingSenderId: "220832175925",
+  appId: "1:220832175925:web:1b4b879e17c4c7e0a7de7c"
+};
 
+initializeApp(firebaseConfig);
+const storage = getStorage();
+const upload = multer();
 Route.post("/Question", (req, res) => {
 
-    const Questions = new Questios(
-        {
-            Question: req.query.Question,
-            Options: req.query.Options,
-            Correct_Option: req.query.Correct_Option,
-        }
-    )
-    Questions.save().then(() => {
-        res.end("Okey Data Has Been Added")
+  const Questions = new Questios(
+    {
+      Question: req.query.Question,
+      Options: req.query.Options,
+      Correct_Option: req.query.Correct_Option,
+    }
+  )
+  Questions.save().then(() => {
+    res.end("Okey Data Has Been Added")
 
-    }).catch((err) => {
-        console.log(err);
-        res.end("Somthing Went Wrong")
+  }).catch((err) => {
+    console.log(err);
+    res.end("Somthing Went Wrong")
 
-    })
-    // res.end("hello")
+  })
 })
 
 
 Route.get("/Question", (req, res) => {
-    Questios.find({}).then((doc) => {
-        res.status(200).json(doc);
-    }).catch((err) => {
-        res.status(500).json(err)
-    })
+  Questios.find({}).then((doc) => {
+    res.status(200).json(doc);
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
 })
-Route.post('/Category',upload.single("image"), (req, res) => {
-  // const pdfPath = req.file.filename;
-  console.log(req.file.path);
-    try {
-        Category.find({ Category_Name: req.body.Category_Name })
-            .then((doc) => {
-                // console.log(doc.length);
-                if (doc.length >= 1) {
-                    res.status(200).json({
-                        status: false,
-                        message: "This Category Already Exist"
-                    });
-                } else {
+Route.post('/Category', upload.single("image"), async (req, res) => {
+  try {
 
-                    const category = new Category({
-                        Category_Name: req.body.Category_Name,
-                        Category_Image:req.file.path
-
-                    });
-                    category
-                        .save()
-                        .then(() => {
-                            res.status(200).json({
-                                status: true,
-                                imagePath:req.file.path,
-                                message: "Category Added"
-                            });
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                            res.status(200).json({
-                                status: false,
-                                message: "Something went Wrong"
-                            });
-                        });
-
-                }
-            })
-
-    } catch (error) {
-        res.status(500).json({
-            status: false,
-            message: "Server Crashed"
-        });
-    }
-})
-Route.get("/Category",(req,res)=>{
-  
-  Category.find({}).then((doc)=>{
-     res.status(200).json({
-        "status":true,
-        data:doc
-
-     })
-  }).catch((err)=>{
-    res.status(200).json({
+    const data = await Category.find({ Category_Name: req.body.Category_Name })
+    if (data.length >= 1) {
+      res.status(200).json({
         status: false,
-        message: err
+        message: "This Category Already Exist"
+      });
+    }
+    const dateTime = giveCurrentDateTime();
+    const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
+    const metadata = {
+      contentType: req.file.mimetype,
+    };
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    const category = new Category({
+      Category_Name: req.body.Category_Name,
+      Category_Image: downloadURL
+
+    });
+    await category.save();
+    return res.send({
+      status: true,
+      imagePath: req.file.path,
+      message: "Category Added"
+    });
+  } catch (error) {
+    return res.status(400).send(error.message)
+  }
+});
+
+Route.get("/Category", (req, res) => {
+  Category.find({}).then((doc) => {
+    res.status(200).json({
+      "status": true,
+      data: doc
+
+    })
+  }).catch((err) => {
+    res.status(200).json({
+      status: false,
+      message: err
     });
   })
 })
 
 
 
-Route.post("/past_paper",upload.single("pdf"), async (req, res) => {
-    try {
-      const { Category_Name, Paper_Pdf, Paper_year, Paper_Name } = req.body;
-      const pdfPath = req.file.filename;
-   
-      const paper = new Paper({
-        Category_Name,
-        Paper_Pdf:pdfPath,
-        Paper_year,
-        Paper_Name,
-      });
-  
-      const savedPaper = await paper.save();
-  
-      res.status(201).json(savedPaper);
-    } catch (error) {
-      console.error("Error creating paper:", error);
-      res.status(500).json({ error: "An error occurred while creating the paper" });
-    }
-  });
-  
-  Route.get("/past_paper", async (req, res) => {
-    try {
-      const papers = await Paper.find();
-      res.json(papers);
-    } catch (error) {
-      console.error("Error retrieving papers:", error);
-      res.status(500).json({ error: "An error occurred while retrieving papers" });
-    }
-  });
+Route.post("/past_paper", upload.single("pdf"), async (req, res) => {
+  try {
+    const { Category_Name, Paper_Pdf, Paper_year, Paper_Name } = req.body;
+    const pdfPath = req.file.filename;
+    const dateTime = giveCurrentDateTime();
+    const storageRef = ref(storage, `files/${req.file.originalname + "       " + dateTime}`);
+    const metadata = {
+      contentType: req.file.mimetype,
+    };
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    const paper = new Paper({
+      Category_Name,
+      Paper_Pdf: downloadURL,
+      Paper_year,
+      Paper_Name,
+    });
+
+    const savedPaper = await paper.save();
+
+    res.status(201).json(savedPaper);
+  } catch (error) {
+    console.error("Error creating paper:", error);
+    res.status(500).json({ error: "An error occurred while creating the paper" });
+  }
+});
+
+Route.get("/past_paper", async (req, res) => {
+  try {
+    const papers = await Paper.find();
+    res.json(papers);
+  } catch (error) {
+    console.error("Error retrieving papers:", error);
+    res.status(500).json({ error: "An error occurred while retrieving papers" });
+  }
+});
 module.exports = Route;
+const giveCurrentDateTime = () => {
+  const today = new Date();
+  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + ' ' + time;
+  return dateTime;
+}
---
+++ modal/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACQGtdj4qFxD6QPiP0omlaOBkz7zTMJMI",
+  authDomain: "ppsc-db25f.firebaseapp.com",
+  projectId: "ppsc-db25f",
+  storageBucket: "ppsc-db25f.appspot.com",
+  messagingSenderId: "220832175925",
+  appId: "1:220832175925:web:1b4b879e17c4c7e0a7de7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
