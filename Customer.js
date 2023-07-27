ef2c238b68347a1a8ce66afe017cea3b56676d1c
Sheraz <bahoo.hub@gmail.com>
2023-07-27 18:18:32 +0500

    Okay Data Has been Completed

--- Customer.js
+++ Customer.js
@@ -2,7 +2,40 @@ const express = require("express");
 const User = require("./modal/User");
 const bcrypt = require("bcrypt");
 const { default: mongoose } = require("mongoose");
+var nodemailer = require('nodemailer');
+
 const Customer = express.Router();
+function generateOTP() {
+  // Generate a random 4-digit number between 1000 and 9999
+  const otp = Math.floor(Math.random() * 9000) + 1000;
+  return otp.toString();
+}
+const SendEmail = (val, val1) => {
+
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'razakhan.yt0012@gmail.com',
+      pass: 'cvmzenrnkczdipxe'
+    }
+  });
+
+  var mailOptions = {
+    from: 'razakhan.yt0012@gmail.com',
+    to: val,
+    subject: 'Sending Email using Node.js',
+    text: val1
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 Customer.post("/Signup", (req, res) => {
   console.log(req.body.username);
   try {
@@ -42,6 +75,7 @@ Customer.post("/Signup", (req, res) => {
 });
 Customer.post("/Login", (req, res) => {
   console.log(req.query.email);
+
   User.find({ email: req.query.email })
     .then((doc) => {
       bcrypt.compare(req.query.password, doc[0].password, (error, result) => {
@@ -68,5 +102,73 @@ Customer.post("/Login", (req, res) => {
       });
     });
 });
+Customer.post("/reset_email", async (req, res) => {
+  console.log(req.query);
+  try {
+    let doc = await User.find({ email: req.query.email })
+    const otp = generateOTP();
+    if (doc.length >= 1) {
+      SendEmail(req.query.email, otp);
+      console.log(otp);
 
+      res.json({
+        status: true,
+        message: "Email  Founded",
+        otp: otp
+      })
+    } else {
+      res.json({
+        status: true,
+        message: "Email Not Found",
+        doc: doc
+      })
+    }
+    res.json(doc)
+  } catch (error) {
+    res.json(error)
+  }
+
+})
+
+
+Customer.post("/new_password",async(req,res)=>{
+try {
+  let doc = await User.find({ email: req.query.email })
+  if (doc.length>=1) {
+    bcrypt.hash(req.query.password, 10,async (err, hash) => {
+      if (err) throw err;
+      const updatedUser = await User.findOneAndUpdate(
+        { email: req.query.email},
+        { password: hash },
+        { new: true } // Setting { new: true } returns the updated document, otherwise, it returns the original document
+      );
+      if(updatedUser){
+        res.json({
+          status:true,
+          message:"Password Updated"
+        })
+      }else{
+        res.json({
+          status:true,
+          message:"Something went Wrong"
+        })
+      }
+    
+    })
+  }else{
+    res.json({
+      status:false,
+      message:"User not Found"
+    })
+  }
+} catch (error) {
+  res.json({
+    status:false,
+    Error:error
+  })
+} 
+
+
+
+})
 module.exports = Customer;
